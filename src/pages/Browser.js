1ce8767261fa55ede59febee8dbd5acd3f71698b
Peter Morrison <peter.morrison@medlior.com>
2021-05-29 15:05:53 -0600

    Upgrade files add browser

---
+++ src/pages/Browser.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function Browser() {
+  // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
