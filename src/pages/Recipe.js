9659d071b7955b7001b11dd1f1263afc47bd5bf9
Peter Morrison <peter.morrison@medlior.com>
2021-12-17 13:37:13 -0700

    Add sub-recipes.

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -24,16 +24,53 @@ const Recipes = ({ recipeData, setRecipe, recipeList }) => {
 
     return (
         <div>
+            {console.log(recipeList.recipes.filter(element => element.name.includes("Char")))}
             <h1>{viewedRecipe.name}</h1>
             <h2>Sources:</h2><ul>{viewedRecipe.sources && viewedRecipe.sources.map(s => <li><span>{s}</span></li>)}</ul>
             <h2>Tags: <br />{viewedRecipe.tags.map(tag =>
                 <Tag key={tag} style={{ marginTop: '1vh', fontFamily: ['Courier New', 'Courier', 'monospace'] }}>{tag}</Tag>)
             }</h2>
 
-            <h2>Instructions:</h2><ol>{viewedRecipe.recipe && viewedRecipe.recipe.map(tag =>
-                <li key={tag.name} style={{ marginTop: '1vh', fontFamily: ['Courier New', 'Courier', 'monospace'] }}>{
+            <h2>Instructions:</h2><ol style={{ marginLeft: '30%', padding: 0, textAlign: 'left' }}>{viewedRecipe.recipe && viewedRecipe.recipe.map(tag =>
+                <li key={tag.name} style={{ marginTop: '1vh', fontFamily: ['Courier New', 'Courier', 'monospace'] }}>
+                    {tag}
+                    <br /><ul>{recipeList.recipes.filter(element => tag.includes(element.name)) && recipeList.recipes.filter(element => tag.includes(element.name)).map(tag2 =>
+                        
+                        tag2.recipe != null ?
+                            tag2.recipe.map(t => 
+                                <li key={t} style={{ marginTop: '1vh', fontFamily: ['Courier New', 'Courier', 'monospace'] }}>{
+                                    t
+
+                                }</li>
+                            )
+                            :
+                            tag2.sources != null ?
+                            tag2.sources.map(t => 
+                                <li key={t} style={{ marginTop: '1vh', fontFamily: ['Courier New', 'Courier', 'monospace'] }}>{
+                                    t
+
+                                }</li>
+                            )
+                            : 
+                            tag2.ingredients.map(t => 
+                                <li key={t} style={{ marginTop: '1vh', fontFamily: ['Courier New', 'Courier', 'monospace'] }}>{
+                                    t
+
+                                }</li>
+                            )
+
+                        /*<li key={tag2.name} style={{ marginTop: '1vh', fontFamily: ['Courier New', 'Courier', 'monospace'] }}>{
+                            tag2.recipe != null ? tag2.recipe : tag2.sources
+
+                        }</li>*/
+
+                    )
+                    }</ul>
+                    {/* {
                     tag
-                }</li>)
+                    
+                } */}
+                </li>)
             }</ol>
 
             {/* <h3>Tags:</h3>{viewedRecipe.tags.map(t => <><span>{t}</span><br/></>)} */}
